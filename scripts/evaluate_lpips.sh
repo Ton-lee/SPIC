ddbb9d458a6e68ca3521e8a641419828f19d5dbb
Ton-Leo <mynameisdqy@gmail.com>
2024-06-07 14:04:29 +0800

    添加初步性能对比结果

--- scripts/evaluate_lpips.sh
+++ scripts/evaluate_lpips.sh
@@ -1,8 +1,8 @@
 name=$1
-step=$2
-test_folder="/home/Users/dqy/Projects/SPIC/Result/${name}/step${step}/samples/"
-gt_folder="/home/Users/dqy/Projects/SPIC/Result/${name}/step${step}/images/"
-save_path="/home/Users/dqy/Projects/SPIC/Result/${name}/step${step}/lpips.json"
+subfolder=$2
+test_folder="/home/Users/dqy/Projects/SPIC/Result/${name}/${subfolder}/samples/"
+gt_folder="/home/Users/dqy/Projects/SPIC/Result/${name}/${subfolder}/images/"
+save_path="/home/Users/dqy/Projects/SPIC/Result/${name}/${subfolder}/lpips.json"
 /home/Users/dqy/miniconda3/envs/SKB/bin/python \
   "/home/Users/dqy/Projects/SKB-compression/scripts/analysis_LPIPS.py" \
   --folder "$test_folder" \
